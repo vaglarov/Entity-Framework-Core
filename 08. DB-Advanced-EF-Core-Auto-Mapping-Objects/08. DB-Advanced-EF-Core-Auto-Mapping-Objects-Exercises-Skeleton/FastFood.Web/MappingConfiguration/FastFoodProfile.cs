d82ebf234fb87c3e09893a2f5780ac5205a98ca0
vaglarov <pasific@abv.bg>
2019-11-13 23:20:37 +0200

    AutoMapper First Fail

---
+++ 08. DB-Advanced-EF-Core-Auto-Mapping-Objects/08. DB-Advanced-EF-Core-Auto-Mapping-Objects-Exercises-Skeleton/FastFood.Web/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,36 @@
+﻿namespace FastFood.Web.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Web.ViewModels.Employees;
+    using Models;
+
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+           this.CreateMap<Position, RegisterEmployeeViewModel>()
+               .ForMember(x => x.PositionName, y => y.MapFrom(x => x.Id));
+
+          
+            //Employee
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionName, y => y.MapFrom(s => s.Name));
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.PositionName, y => y.MapFrom(s => s.Position.Name));
+
+
+        }
+    }
+}
