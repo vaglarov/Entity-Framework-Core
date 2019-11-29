012eb130fe6203ef5af74a69554f5b20cffd6225
vaglarov <pasific@abv.bg>
2019-11-29 21:32:49 +0200

    new

---
+++ Prepare Exam/01.09.2018/01. Model Definition_Skeleton + Datasets/VaporStore/DataProcessor/ExpotDto/ExportUserDto.cs
@@ -0,0 +1,57 @@
+﻿using System.Xml.Serialization;
+
+namespace VaporStore.DataProcessor.ExpotDto
+{
+    [XmlType("User")]
+    public class ExportUserDto
+    {
+        [XmlAttribute("username")]
+        public string Username { get; set; }
+
+        [XmlArray("Purchases")]
+        public ExportPurchaseDto[] Purchases { get; set; }
+
+        [XmlElement("TotalSpent")]
+        public decimal TotalSpet { get; set; }
+    }
+
+
+    [XmlType("Purchase")]
+    public class ExportPurchaseDto
+    {
+      //         <Card>7991 7779 5123 9211</Card>
+      // <Cvc>340</Cvc>
+      // <Date>2017-08-31 17:09</Date>
+      // <Game title = "Counter-Strike: Global Offensive" >
+      //   < Genre > Action </ Genre >
+      //   < Price > 12.49 </ Price >
+      [XmlElement("Card")]
+        public string Card { get; set; }
+
+        [XmlElement("Cvc")]
+        public string  Cvc { get; set; }
+
+        [XmlElement("Date")]
+        public string Date { get; set; }
+
+        [XmlElement("Game")]
+        public ExportGameDto Game { get; set; }
+
+    }
+
+    [XmlType("Game")]
+    public class ExportGameDto
+    {
+        //   < Genre > Action </ Genre >
+        //   < Price > 12.49 </ Price >
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+
+        [XmlElement("Genre")]
+        public string Genre { get; set; }
+
+        [XmlElement("Price")]
+        public decimal Price { get; set; }
+
+    }
+}
--- Prepare Exam/01.09.2018/01. Model Definition_Skeleton + Datasets/VaporStore/DataProcessor/Serializer.cs
+++ Prepare Exam/01.09.2018/01. Model Definition_Skeleton + Datasets/VaporStore/DataProcessor/Serializer.cs
@@ -1,18 +1,106 @@
 ﻿namespace VaporStore.DataProcessor
 {
-	using System;
-	using Data;
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
+    using System.Xml.Serialization;
+    using Data;
+    using Newtonsoft.Json;
+    using VaporStore.Data.Models.Enums;
+    using VaporStore.DataProcessor.ExpotDto;
 
-	public static class Serializer
-	{
-		public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
-		{
-			throw new NotImplementedException();
-		}
+    public static class Serializer
+    {
+        //       "Id": 4,
+        //"Genre": "Violent",
+        //"Games": [
+        //  {
+        //    "Id": 49,
+        //    "Title": "Warframe",
+        //    "Developer": "Digital Extremes",
+        //    "Tags": "Single-player, In-App Purchases, Steam Trading Cards, Co-op, Multi-player, Partial Controller Support",
+        //    "Players": 6
 
-		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
-		{
-			throw new NotImplementedException();
-		}
-	}
+
+        //  },
+        //"TotalPlayers": 10
+
+        public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
+        {
+            var genres = context
+                .Genres
+                .Where(g => genreNames.Contains(g.Name))
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Genre = x.Name,
+                    Games = x.Games
+                    .Where(p => p.Purchases.Any())
+                    .Select(y => new
+                    {
+                        Id = y.Id,
+                        Title = y.Name,
+                        Deleloper = y.Developer.Name,
+                        Tags = string.Join(", ", y.GameTags.Select(t => new { t.Tag.Name })),
+                        Players = y.Purchases.Count()
+                    })
+                    .OrderByDescending(z => z.Players)
+                    .ThenBy(s => s.Id)
+                    .ToArray(),
+                    TotalPlayers = x.Games.Sum(p => p.Purchases.Count)
+                })
+                .OrderByDescending(p => p.TotalPlayers)
+                .ThenBy(g => g.Id)
+                .ToArray();
+
+            var jsonResult = JsonConvert.SerializeObject(genres, Newtonsoft.Json.Formatting.Indented);
+
+            return jsonResult;
+        }
+
+        public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
+        {
+            var enumType = Enum.Parse<PurchaseType>(storeType);
+
+            var users = context.Users
+                .Select(x => new ExportUserDto
+                {
+                    Username = x.Username,
+                    Purchases = x.Cards.SelectMany(p => p.Purchases).Where(p=>p.Type== enumType).Select(c => new ExportPurchaseDto
+                    {
+
+                        Card = c.Card.Number,
+                        Cvc = c.Card.Cvc,
+                        Date = c.Date.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+                        Game = new ExportGameDto
+                        {
+                            Genre = c.Game.Genre.Name,
+                            Title = c.Game.Name,
+                            Price = c.Game.Price
+                        }
+                    })
+                        .OrderBy(c => c.Date)
+                        .ToArray(),
+                    TotalSpet = x.Cards.SelectMany(p => p.Purchases).Sum(p => p.Game.Price)
+                })
+                .OrderByDescending(t => t.TotalSpet)
+                .ThenBy(u => u.Username)
+                .ToArray();
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportUserDto[]), new XmlRootAttribute("Users"));
+
+            var sb = new StringBuilder();
+
+            var namespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty});
+
+            xmlSerializer.Serialize(new StringWriter(sb), users, namespaces);
+
+            var result = sb.ToString().TrimEnd();
+
+            return result;
+        }
+    }
 }
